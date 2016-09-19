f97b2e9ce4b6d580b76f5b2f2be74837ae39c9ab
Maciej Kurek <maciejk77@gmail.com>
2016-09-19 21:11:38 +0100

    YT api search installed & google api key set up

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+const API_KEY = 'AIzaSyAtwCu0gfBJxnPCUt5-qx15kEipJTOxKf8';
+
 const App = () => {
   return <div>PandaTube</div>;
 };
