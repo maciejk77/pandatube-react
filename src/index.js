70b35e755e9d4350744d1d9a404143d09692cb53
Maciej Kurek <maciejk77@gmail.com>
2016-09-19 21:32:26 +0100

    Search bar added

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import SearchBar from './components/search_bar';
 
 const API_KEY = 'AIzaSyAtwCu0gfBJxnPCUt5-qx15kEipJTOxKf8';
 
 const App = () => {
-  return <div>PandaTube</div>;
+  return (
+    <div>
+      <SearchBar />
+    </div>
+  )
 };
 
 ReactDOM.render(<App />, document.querySelector('.container'));
