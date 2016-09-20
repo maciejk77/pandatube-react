9d7745a539959fc2d0cf1dd3ebe9c2d653ce1670
Maciej Kurek <maciejk77@gmail.com>
2016-09-20 07:13:05 +0100

    List of videos and description rendering

--- src/index.js
+++ src/index.js
@@ -2,17 +2,25 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
 const API_KEY = 'AIzaSyAtwCu0gfBJxnPCUt5-qx15kEipJTOxKf8';
 
-YTSearch({key: API_KEY, term: 'panda'}, function(data) {
-  console.log(data);
-});
-
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { videos: [] };
+    
+    YTSearch({key: API_KEY, term: 'panda'}, (videos) => {
+      this.setState({ videos });
+    });
+  }
+
   render() {
     return (
       <div>
         <SearchBar />
+        <VideoList videos={this.state.videos} />
       </div>
     )
   }
