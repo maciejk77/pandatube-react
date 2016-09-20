3d89dd72f401a8220abb142f7d35d1f35b147189
Maciej Kurek <maciejk77@gmail.com>
2016-09-20 06:18:08 +0100

    Refactored App component to class based

--- src/index.js
+++ src/index.js
@@ -1,15 +1,21 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
-
 const API_KEY = 'AIzaSyAtwCu0gfBJxnPCUt5-qx15kEipJTOxKf8';
 
-const App = () => {
-  return (
-    <div>
-      <SearchBar />
-    </div>
-  )
+YTSearch({key: API_KEY, term: 'panda'}, function(data) {
+  console.log(data);
+});
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <SearchBar />
+      </div>
+    )
+  }
 };
 
 ReactDOM.render(<App />, document.querySelector('.container'));
