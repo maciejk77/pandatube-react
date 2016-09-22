107e6c5d7557e821ceb9152bd9b6afd3eefa5d74
Maciej Kurek <maciejk77@gmail.com>
2016-09-22 21:21:31 +0100

    Logo and a bit of styling completed

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -10,7 +10,7 @@ class SearchBar extends Component {
   render() { 
     return (
       <div className="search-bar">
-        <input 
+        <input
           value={this.state.term}
           onChange={event => this.onInputChange(event.target.value)} />
       </div>
